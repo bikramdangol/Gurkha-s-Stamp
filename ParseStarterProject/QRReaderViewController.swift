0bded690bdfbcd89415246f0bfd6673944982c9e
Bikram Dangol <Bikram.Dangol@davita.com>
2015-12-10 16:19:04 -0700

    Added QR reader functionality.

--- ParseStarterProject/QRReaderViewController.swift
+++ ParseStarterProject/QRReaderViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 import Parse
+import AVFoundation
 
-class QRReaderViewController: UIViewController {
+class QRReaderViewController: UIViewController, QRCodeReaderViewControllerDelegate  {
+    lazy var reader = QRCodeReaderViewController(metadataObjectTypes: [AVMetadataObjectTypeQRCode])
 
     @IBOutlet var stampedMessageLabel: UILabel!
     @IBOutlet var stampButton: UIButton!
@@ -37,6 +39,31 @@ class QRReaderViewController: UIViewController {
     @available(iOS 8.0, *)
     func stampIt()
     {
+        if QRCodeReader.supportsMetadataObjectTypes() {
+            reader.modalPresentationStyle = .FormSheet
+            reader.delegate               = self
+            
+            reader.completionBlock = { (result: String?) in
+                print("Completion with result: \(result)")
+                self.stampedMessageLabel.text = result
+                if result == "Bikram is learning iOS."
+                {
+                    self.saveStamp()
+                }
+            }
+            
+            presentViewController(reader, animated: true, completion: nil)
+        }
+        else {
+            let alert = UIAlertController(title: "Error", message: "Reader not supported by the current device", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+            
+            presentViewController(alert, animated: true, completion: nil)
+        }
+
+    }
+    func saveStamp()
+    {
         let stamp = PFObject(className:"Stamp")
         stamp["user"] = PFUser.currentUser()
         stamp.saveInBackgroundWithBlock {
@@ -47,12 +74,40 @@ class QRReaderViewController: UIViewController {
                 self.stampedMessageLabel.text = "Stamped!!!"
             } else {
                 // There was a problem, check error.description
-                let alert = UIAlertController(title: "Error", message: "Something went wrong. Please scan the QR Code again.", preferredStyle: UIAlertControllerStyle.Alert)
-                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-
+                if #available(iOS 8.0, *) {
+                    let alert = UIAlertController(title: "Error", message: "Something went wrong. Please scan the QR Code again.", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+                } else {
+                    // Fallback on earlier versions
+                }
+                
+                
             }
         }
+
     }
+    
+    // MARK: - QRCodeReader Delegate Methods
+    
+    
+    func reader(reader: QRCodeReaderViewController, didScanResult result: String) {
+        self.dismissViewControllerAnimated(true, completion: { [unowned self] () -> Void in
+            if #available(iOS 8.0, *) {
+                let alert = UIAlertController(title: "QRCodeReader", message: result, preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+                
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                // Fallback on earlier versions
+            }
+            
+            })
+    }
+    
+    func readerDidCancel(reader: QRCodeReaderViewController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
 
     /*
     // MARK: - Navigation
